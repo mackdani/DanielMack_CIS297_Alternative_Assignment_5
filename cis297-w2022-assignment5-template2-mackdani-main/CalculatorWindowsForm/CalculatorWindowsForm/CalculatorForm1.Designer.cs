a14ba94fbf2744b913042e9611018b89449efeb0
mackdani <98480193+mackdani@users.noreply.github.com>
2022-04-14 16:08:50 -0400

    Add files via upload

---
+++ cis297-w2022-assignment5-template2-mackdani-main/CalculatorWindowsForm/CalculatorWindowsForm/CalculatorForm1.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace CalculatorWindowsForm
+{
+    partial class CalculatorForm1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CalculatorForm1));
+            this.Result_TextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button11 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Result_TextBox
+            // 
+            this.Result_TextBox.Location = new System.Drawing.Point(12, 96);
+            this.Result_TextBox.Name = "Result_TextBox";
+            this.Result_TextBox.Size = new System.Drawing.Size(474, 20);
+            this.Result_TextBox.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Result";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(9, 139);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(581, 208);
+            this.label5.TabIndex = 8;
+            this.label5.Text = resources.GetString("label5.Text");
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Location = new System.Drawing.Point(746, 139);
+            this.button3.Margin = new System.Windows.Forms.Padding(0);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(63, 50);
+            this.button3.TabIndex = 31;
+            this.button3.Text = "3";
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Location = new System.Drawing.Point(683, 139);
+            this.button2.Margin = new System.Windows.Forms.Padding(0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(63, 50);
+            this.button2.TabIndex = 30;
+            this.button2.Text = "2";
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(620, 139);
+            this.button1.Margin = new System.Windows.Forms.Padding(0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(63, 50);
+            this.button1.TabIndex = 29;
+            this.button1.Text = "1";
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button4
+            // 
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button4.Location = new System.Drawing.Point(620, 189);
+            this.button4.Margin = new System.Windows.Forms.Padding(0);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(63, 50);
+            this.button4.TabIndex = 34;
+            this.button4.Text = "4";
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button5.Location = new System.Drawing.Point(683, 189);
+            this.button5.Margin = new System.Windows.Forms.Padding(0);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(63, 50);
+            this.button5.TabIndex = 33;
+            this.button5.Text = "5";
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button6.Location = new System.Drawing.Point(746, 189);
+            this.button6.Margin = new System.Windows.Forms.Padding(0);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(63, 50);
+            this.button6.TabIndex = 32;
+            this.button6.Text = "6";
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button7.Location = new System.Drawing.Point(620, 239);
+            this.button7.Margin = new System.Windows.Forms.Padding(0);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(63, 50);
+            this.button7.TabIndex = 37;
+            this.button7.Text = "7";
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button8.Location = new System.Drawing.Point(683, 239);
+            this.button8.Margin = new System.Windows.Forms.Padding(0);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(63, 50);
+            this.button8.TabIndex = 36;
+            this.button8.Text = "8";
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button9.Location = new System.Drawing.Point(746, 239);
+            this.button9.Margin = new System.Windows.Forms.Padding(0);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(63, 50);
+            this.button9.TabIndex = 35;
+            this.button9.Text = "9";
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button10.Location = new System.Drawing.Point(683, 289);
+            this.button10.Margin = new System.Windows.Forms.Padding(0);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(63, 50);
+            this.button10.TabIndex = 38;
+            this.button10.Text = "10";
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 39;
+            this.label1.Text = "1st Input";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(277, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 40;
+            this.label2.Text = "2nd Input";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(555, 17);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 41;
+            this.label3.Text = "3rd Input";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(229, 20);
+            this.textBox1.TabIndex = 42;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(280, 33);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(229, 20);
+            this.textBox2.TabIndex = 43;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(558, 33);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(229, 20);
+            this.textBox3.TabIndex = 44;
+            // 
+            // button11
+            // 
+            this.button11.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button11.Location = new System.Drawing.Point(620, 80);
+            this.button11.Margin = new System.Windows.Forms.Padding(0);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(126, 50);
+            this.button11.TabIndex = 45;
+            this.button11.Text = "CLEAR";
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // CalculatorForm1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(855, 450);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Result_TextBox);
+            this.Name = "CalculatorForm1";
+            this.Text = "Calculator GUI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox Result_TextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button11;
+    }
+}
+
---
+++ cis297-w2022-assignment5-template2-mackdani-main/CalculatorWindowsForm/CalculatorWindowsForm/CalculatorForm1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorWindowsForm
+{
+    /// </summary>
+    /// <Student>Daniel Mack</Student>
+    /// <Class>CIS297</Class>
+    /// <Semester>Winter 2022</Semester>
+    public partial class CalculatorForm1 : Form
+    {
+        public CalculatorForm1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 1.	Return a string without the Whitespaces
+        /// </summary>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string Textinput = textBox1.Text;
+            Result_TextBox.Text = Textinput.Replace(" ", "");
+        }
+        /// <summary>
+        /// 2.	Reverse a string
+        /// </summary>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            char[] charArray = textBox1.Text.ToCharArray();
+            Array.Reverse(charArray);
+            string result;
+            result = new string(charArray);
+            Result_TextBox.Text = result;
+        }
+        /// <summary>
+        /// 3.	3) Display the quotient and the remainder
+        /// </summary>
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int firstnum = int.Parse(textBox1.Text);
+            int secondnum = int.Parse(textBox2.Text);
+            int remainder;
+            double quotient = Math.DivRem(firstnum, secondnum, out remainder);
+            Result_TextBox.Text = $"Quotient:{quotient}                         Remainder:{remainder}";
+        }
+        /// </summary>
+        /// 4) Find the logarithm of a specified number to base 10
+        /// </summary>
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double firstnum = double.Parse(textBox1.Text);
+            double Log = Math.Log10(firstnum);
+            Result_TextBox.Text = $"Log({firstnum}) to the base 10 is {Log}";
+        }
+        /// </summary>
+        /// 5) Find the logarithm of a specified number to a specified base
+        /// </summary>
+        private void button5_Click(object sender, EventArgs e)
+        {
+            double firstnum = double.Parse(textBox1.Text);
+            double secondnum = double.Parse(textBox2.Text);
+            double LogBase = Math.Log(firstnum, secondnum);
+            Result_TextBox.Text = $"Log({firstnum}) to the base {secondnum} is {LogBase}";
+        }
+        /// </summary>
+        /// 6) Display the minimum and maximum of the two numbers
+        /// </summary>
+        private void button6_Click(object sender, EventArgs e)
+        {
+            double num1 = Double.Parse(textBox1.Text);
+            double num2 = Double.Parse(textBox2.Text);
+
+            double min1 = Math.Min(num1, num2);
+            double max1 = Math.Max(num1, num2);
+
+            Result_TextBox.Text = $"Minimum: {min1}    &    Maximum: { max1 }";
+        }
+        /// </summary>
+        /// 7) Find the specified number to the specified power
+        /// </summary>
+        private void button7_Click(object sender, EventArgs e)
+        {
+            double firstnum = double.Parse(textBox1.Text);
+            double secondnum = double.Parse(textBox2.Text);
+            double Result = Math.Pow(firstnum, secondnum);
+            Result_TextBox.Text = $"{firstnum} to the power {secondnum} is {Result}";
+        }
+        /// </summary>
+        /// 8) Find the roots of a quadratic equation
+        /// </summary>
+            private void button8_Click(object sender, EventArgs e)
+        {
+            double discrim;
+            double X_1;
+            double X_2;
+            double a = double.Parse(textBox1.Text);
+            double b = double.Parse(textBox2.Text);
+            double c = double.Parse(textBox3.Text);
+            discrim = (b * b) - (4 * a * c);
+            if (discrim == 0)
+            {
+                X_1 = -b / (2 * a);
+                X_2 = X_1;
+                Result_TextBox.Text = $"Root X1 = {X_1}  &  Root X2 = {X_2}";
+            }
+            else if (discrim > 0)
+            {
+                X_1 = (-b + Math.Sqrt(discrim)) / (2 * a);
+                X_2 = (-b - Math.Sqrt(discrim)) / (2 * a);
+                Result_TextBox.Text = $"Root X1 = {X_1}  &  Root X2 = {X_2}";
+            }
+        }
+        /// </summary>
+        /// 9) Find the square root of the given number
+        /// </summary>
+        private void button9_Click(object sender, EventArgs e)
+        {
+            double number = double.Parse(textBox1.Text);
+            Result_TextBox.Text = Math.Sqrt(number).ToString();
+        }
+        /// <summary>
+        /// 10) Exit
+        /// </summary>
+        private void button10_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            Result_TextBox.Text = "";
+        }
+    }
+}
