cd7476ceb753b84ce9d9082e7a08544e77a9490a
mackdani <98480193+mackdani@users.noreply.github.com>
2022-04-14 16:10:07 -0400

    Add files via upload

---
+++ cis297-w2022-assignment5-template2-mackdani-main/CalculatorWindowsForm/CalculatorWindowsForm/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorWindowsForm
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// This program implements some arithmetic functions as a calculator
+        /// And uses the Window Forms App to perform the functions of a calculator
+        /// </summary>
+        /// <Student>Daniel Mack</Student>
+        /// <Class>CIS297</Class>
+        /// <Semester>Winter 2022</Semester>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new CalculatorForm1());
+        }
+    }
+}
